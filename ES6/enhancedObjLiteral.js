5caa49b08a2cf30bce350a081a31422b088c8754
Brian Haller <brianhaller@Brians-MacBook-Pro.local>
2018-05-16 09:53:37 -0700

    commit

--- ES6/enhancedObjLiteral.js
+++ ES6/enhancedObjLiteral.js
@@ -1,10 +1,5 @@
 // enhanced object literals
-// this is just syntactic sugar for succinct objects
-// if a key and a value are identical, you just have to use the key and value word once,
-// if a value is a function, you don't have to write key: function(){}. Instead, you can just give the function a name and omit the colon. So you just write functionName(){...}
-// A place where you see both these techniques is in react.
-
-//There are 2 new syntaxes for nice succinct objects
+// There are 2 new syntaxes for nice succinct objects
 
 //function definitions in objects
 //Functions in objects don't require a key name or the function keyword.
