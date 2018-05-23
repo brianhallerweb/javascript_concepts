d862688bb268f60cdd34e43faf2c8631e1376938
Brian Haller <brianhaller@Brians-MacBook-Pro.local>
2018-05-23 12:51:08 -0700

    commit

--- closure/module_pattern.js
+++ closure/module_pattern.js
@@ -1,6 +1,10 @@
-// The module pattern is the most important code organization pattern in js. It was created by Crockford in the early 2000s and its purpose is to hide and prevent access to complexity. Modules follow the "principle of least exposure" - they hide and make inaccessible the inner workings of a public API.
+// The module pattern is the most important code organization pattern in js. It was created
+// by Crockford in the early 2000s and its purpose is to hide and prevent access to
+// complexity. Modules follow the "principle of least exposure" - they hide and make
+// inaccessible the inner workings of a public API.
 
-// Modules are characterized by having an enclosing scope and at least 1 function that closes over variables in that state.
+// Modules are characterized by having an enclosing scope and at least 1 function that closes
+// over variables in that state.
 
 // This code isn't a module because everything in foo is exposed.
 var foo = {
@@ -23,7 +27,8 @@ var foo = (function() {
 })();
 foo.bar();
 
-// This is a variant of the classic module pattern that allows access to the public API from within the module.
+// This is a variant of the classic module pattern that allows access to the public API from
+// within the module.
 var foo = (function() {
   var publicAPI = {
     bar: function() {
@@ -38,7 +43,14 @@ var foo = (function() {
 foo.bar();
 
 // Singletons vs Module Factory Functions
-// When a module is created with an IFFE it will only run once. This pattern is called a Singeton because it is limited to creating a single module. If a module is written as a function that can be repeatedly called, it is called a Module Factory Function.
+// When a module is created with an IFFE it will only run once. This pattern is called a
+// Singeton because it is limited to creating a single module. If a module is written as a
+// function that can be repeatedly called, it is called a Module Factory Function.
 
 // ES6 Modules
-// It is good to think of ES6 modules as native module support. It allows each file to be treated as a private module. What is made public (exported) must be explicitly declared. But there are problems with ES6 modules. Although they are in the official spec, they are relatively unstable because incompatibilities with existing npm packages and node itself is proving to be a huge problem. I believe they are now nearing a solution but ES6 modules may have to change a little as a result.
+// It is good to think of ES6 modules as native module support. It allows each file to be
+// treated as a private module. What is made public (exported) must be explicitly declared.
+// But there are problems with ES6 modules. Although they are in the official spec, they are
+// relatively unstable because incompatibilities with existing npm packages and node itself
+// is proving to be a huge problem. I believe they are now nearing a solution but ES6 modules
+// may have to change a little as a result.
