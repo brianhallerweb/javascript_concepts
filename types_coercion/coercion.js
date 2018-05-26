9ed7ba5231ace192ea3a83cac8a0f6c32e1a22cb
Brian Haller <brianhaller@Brians-MacBook-Pro.local>
2018-05-25 20:16:08 -0700

    commit

--- types_coercion/coercion.js
+++ types_coercion/coercion.js
@@ -1,5 +1,8 @@
 ////////// Abstract Operations //////////////
-//These operators aren't accessible for use but they are important because they are run behind-the-scenes for all type conversions. They inlude ToPrimitive, ToString, ToNumber, ToBoolean. A lot of unexpected results originate in how these abstract operators work. The big one to memorize is that ToNumber("") returns 0.
+// These operators aren't accessible for use but they are important because they are run
+// behind-the-scenes for all type conversions. They inlude ToPrimitive, ToString, ToNumber,
+// ToBoolean. A lot of unexpected results originate in how these abstract operators work. The big
+// one to memorize is that ToNumber("") returns 0.
 
 ////////// Explicit coercion //////////
 
@@ -7,7 +10,8 @@
 // This is commonly done with parseInt but parseInt isn't true coercion, it just parses a string.
 var foo = "123x2";
 var baz = parseInt(foo);
-baz; // 123, because parseInt loops through a string looking for integers. It stops when it encounters a non integer.
+baz; // 123, because parseInt loops through a string looking for integers. It stops when it
+// encounters a non integer.
 // Use the Number native function or the + operator instead.
 // Number native function
 var foo = "123";
@@ -19,7 +23,10 @@ var baz = 1 + +foo;
 baz; //124
 
 //number to string
-//Either use the String native function or the .toString method available on numbers. How does a number have a method on it? When methods are called on numbers, the numbers undergo implicit "boxing", which just means the number gets wrapped in the same object that is created when the Number native function is used with the new keyword. Don't worry too much about this detail...
+//Either use the String native function or the .toString method available on numbers. How does a
+// number have a method on it? When methods are called on numbers, the numbers undergo implicit
+// "boxing", which just means the number gets wrapped in the same object that is created when the
+// Number native function is used with the new keyword. Don't worry too much about this detail...
 var baz = 456;
 var foo = String(baz);
 foo; //"456"
@@ -34,7 +41,10 @@ typeof foo; // string
 var foo = "123";
 var baz = !!foo;
 baz; //true
-//The above is a very common way to create booleans. It works fine but why not use the Boolean native function? It is surely clearer that way and it works more directly. Using a double bang both coerces and negates, whereas using the Boolean native function just coerces. Do this instead:
+//The above is a very common way to create booleans. It works fine but why not use the Boolean
+// native function? It is surely clearer that way and it works more directly. Using a double bang
+// both coerces and negates, whereas using the Boolean native function just coerces. Do this
+// instead:
 var foo = "123";
 var baz = Boolean(foo);
 baz; // true
@@ -43,7 +53,8 @@ baz; // true
 // The Date constructor returns something like this:
 var now = new Date();
 now; //Thu May 24 2018 14:02:00 GMT-0700 (PDT)
-// The most common use of the Date constructor is to create a numeric timestamp. This can be easily done 2 ways.
+// The most common use of the Date constructor is to create a numeric timestamp. This can be easily
+// done 2 ways.
 var now = new Date();
 now = now.getTime();
 now; //1527195878550
@@ -65,7 +76,9 @@ var foo = "123";
 var baz = foo - 0;
 baz; // 123
 typeof baz; //number
-// There are many mathematical operators that also work but keep in mind that if either operand is a string, the + operator always converts the operand to a string. So, use the + for implicitly converting from number to string, like shown below.
+// There are many mathematical operators that also work but keep in mind that if either operand is
+// a string, the + operator always converts the operand to a string. So, use the + for implicitly
+// converting from number to string, like shown below.
 
 // number to string
 var foo = 456;
@@ -82,24 +95,33 @@ var bar = 0;
 if (bar) {
   console.log("won't run");
 }
-//Conditionals are always implicitly coerced to Booleans. Remember that any value not on the falsy list is coerced to true.
+//Conditionals are always implicitly coerced to Booleans. Remember that any value not on the falsy
+// list is coerced to true.
 
 // Double equals
-// It is commonly said that double equals evaluates value while triple equals evaluates value and type. This statement is simply false and proof comes straight from the spec.
-// The correct statement that while they both test equality, double equals allows type coercion and triple equals doesn't. //Double equals follows understandable rules
+// It is commonly said that double equals evaluates value while triple equals evaluates value and
+// type. This statement is simply false and proof comes straight from the spec.
+// The correct statement that while they both test equality, double equals allows type coercion and
+// triple equals doesn't. //Double equals follows understandable rules
 //1. if both values are the same type, check for strict equality with ===
-//2. null and undefined are equal to each other and no other values. This one is very useful to know because null and undefined are conceptually very similar, they are both empty values. Coercion is genuinely useful here.
+//2. null and undefined are equal to each other and no other values. This one is very useful to
+// know because null and undefined are conceptually very similar, they are both empty values.
+// Coercion is genuinely useful here.
 //3. if one is a number and the other a string, convert the string to a number
 //4. if one is a number and the other a boolean, convert the boolean to a number
-//5. if one is a number and the other is an array or object, call the abstract operator ToPrimitive on the object or array. That results in NaN for objects and "" for arrays, which is then converted to a number and Number("") return 0.
+//5. if one is a number and the other is an array or object, call the abstract operator ToPrimitive
+// on the object or array. That results in NaN for objects and "" for arrays, which is then
+// converted to a number and Number("") return 0.
 
 //Double equals edge cases with explanation
-null == undefined; // true but not because both are coverted to the number 0. It is because null and undefined are special cases, they are coercively equal to each other and no other value.
+null == undefined; // true but not because both are coverted to the number 0. It is because null
+// and undefined are special cases, they are coercively equal to each other and no other value.
 "0" == null; //false because null is only coercively equal to undefined.
 "0" == false; //true because both are coverted to 0
 "0" == NaN; //false because NaN is a number and 0 != NaN
 "0" == 0; //true because "0" is coverted to 0
-"0" == ""; //false but I'm not sure why - I would think both would be converted to 0 - The problem is that if both values are the same type, no conversion happens
+"0" == ""; //false but I'm not sure why - I would think both would be converted to 0 - The problem
+// is that if both values are the same type, no conversion happens
 false == null; //false because null and undefined are special values
 false == undefined; //false because null and undefined are special values
 false == NaN; //false because NaN is already a number
--- types_coercion/logicalAndOr.js
+++ types_coercion/logicalAndOr.js
@@ -1,4 +1,39 @@
-// var foo = "";
-// var baz = foo || "foo";
-// console.log(baz); //"foo"
-// The above example is really important. Logical OR and logical AND operators are misnamed. They are not logical operators because they do not always return a boolean. They are better thought of as selection operators. They take 2 operands - they do a boolean coercion test against the first one, then they select one of the 2. For OR, if the first is true, it is selected. If false, the second operand is selected.
+// Logical And and OR
+
+// Logical OR and logical AND operators are misnamed. They are not logical operators because they do not always return a boolean. They are better thought of as selection operators.
+
+////////// OR /////////////
+
+//If the first is false after boolean coercion, the second is selected.
+var foo = "";
+var baz = foo || "second";
+baz; //"second"
+var foo = "";
+var baz = foo || 0;
+baz; //0
+
+//If the first is true after boolean coercion, it is selected
+var foo = "first";
+var baz = foo || "second";
+baz; //"first"
+var foo = 1;
+var baz = foo || 0;
+baz; //1
+
+////////// AND /////////////
+
+//If the first is true after boolean coercion, the second is selected.
+var foo = "first";
+var baz = foo && "second";
+baz; //"second"
+var foo = 1;
+var baz = foo && 0;
+baz; //0
+
+//If the first is false after boolean coercion, the first is selected.
+var foo = 0;
+var baz = foo && "second";
+baz; //0
+var foo = "";
+var baz = foo && 0;
+baz; //""
